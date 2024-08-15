607e981f86bb0d5b0ec7bde37629601b510e1486
Mihail Solovev <mikhail1solovev@gmail.com>
2024-08-15 17:53:55 +0500

    Commit

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import UserRegisterView, PasswordResetView, HomePageView, ProfileView, LogoutView
 from django.contrib.auth import views as auth_views
-from django.urls import path
-from .views import RegisterView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
@@ -15,5 +13,3 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
-
-
