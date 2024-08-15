32c6e9c50e1b56444a953544aa203e949d9e6fbd
Mihail Solovev <mikhail1solovev@gmail.com>
2024-08-15 18:26:57 +0500

    Commit

--- myproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from users.views import HomePageView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('', HomePageView.as_view(), name='home'),
-]
