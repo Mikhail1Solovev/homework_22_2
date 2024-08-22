f1074d9c2006f80524d0a55925a8aa3971b7ea34
Mihail Solovev <mikhail1solovev@gmail.com>
2024-08-22 18:07:11 +0500

    Initial commit

--- products/models.py
+++ products/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from django.conf import settings
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     description = models.TextField()
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='product_owner')
+    category = models.CharField(max_length=255, default='Uncategorized')
+    is_published = models.BooleanField(default=False)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='product_owner')  # Владелец продукта
+
+    class Meta:
+        permissions = [
+            ("can_unpublish_product", "Может отменить публикацию продукта"),
+            ("can_change_product_description", "Может менять описание любого продукта"),
+            ("can_change_product_category", "Может менять категорию любого продукта"),
+        ]
 
     def __str__(self):
-        return self.name
-
+        return self.title
