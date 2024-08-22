c92e67848c3d082a91910a16b3fd07e6d62feeab
Mihail Solovev <mikhail1solovev@gmail.com>
2024-08-22 19:01:55 +0500

    Initial commit

--- users/urls.py
+++ users/urls.py
@@ -4,15 +4,14 @@ from . import views
 from .views import EmailVerificationNeededView
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup'),
     path('register/', views.SignUpView.as_view(), name='register'),
     path('verify/<uidb64>/<token>/', views.verify_email, name='verify_email'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('reset_password/', views.reset_password, name='reset_password'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     path('products/', views.ProductListView.as_view(), name='product_list'),
     path('products/create/', views.ProductCreateView.as_view(), name='product_create'),
     path('products/<pk>/update/', views.ProductUpdateView.as_view(), name='product_update'),
